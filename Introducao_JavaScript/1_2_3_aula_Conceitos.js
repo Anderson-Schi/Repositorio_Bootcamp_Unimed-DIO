31377f0f37c0959b6d257f03a897e65022323f12
Anderson-Schi <jr93@hotmai.com>
2022-06-16 23:51:34 +0100

    Aulas Introducao JS

---
+++ Introducao_JavaScript/1_2_3_aula_Conceitos.js
@@ -0,0 +1,37 @@
+// Aulas 1, 2 e 3 do curso 
+
+// var y = 0; -----> Comentario de linha (ctrl + /)
+
+/*var x = 1;  
+  var z = 2   ----->  Comentario de bloco / varias linhas 
+  */
+// ----------------------------------------------------------------------------
+// O que e uma variavel ? 
+
+//  Funcao do primeiro grau
+
+  // ax + b = 0 ((a e b = constantes x = variavel))
+
+      // f(1) = a.1 + b 
+      // f(2+3) = a.(2+3) + b
+      // f(9) = a.9 + b
+      // f(x) = 2x + 3
+// ------------------------------------------------------------------------------
+
+// constante em JS e declarado em letra maiscula
+
+// var preco = 2;
+// var desconto = 0.2;
+// const PRECO = 2;
+// var total = PRECO - desconto;
+
+// --------------------------------------------------------------------------
+
+// Funcoes
+
+// function soma(a, b) {   ((a e b sao parametros))
+//   return a + b; // retorna o resultado 8
+// }
+
+// soma(3, 5);
+
